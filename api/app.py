ce192922ab8273b03a2a700614bdc9856c0e5716
Jc4dMn <guillaumecarcema@gmail.com>
2021-02-11 15:44:59 +0100

    Added Api's views

--- api/app.py
+++ api/app.py
@@ -1,8 +1,10 @@
 import os
 
-from flask import request, url_for
+from flask import request, url_for, jsonify
 from flask_api import FlaskAPI, status, exceptions
 from flask_sqlalchemy import SQLAlchemy
+
+from flask_script import Manager
 
 app = FlaskAPI(__name__)
 
@@ -13,13 +15,38 @@ db = SQLAlchemy(app)
 
 from models.models import Food
 
-@app.route("/api")
-def api_details():
-    """Returns api"""
-    # Doing database first. Oups forgot.
-    return {
-        random : "test"
-    }
+@app.route("/add")
+def add_food():
+    name=request.args.get('name')
+    count=request.args.get('count')
+    try:
+        food=Food(
+            name=name,
+            count=count,
+        )
+        db.session.add(food)
+        db.session.commit()
+        return "Food added. food id={}".format(food.id)
+    except Exception as e:
+        return(str(e))
+
+@app.route("/getall")
+def get_all():
+    """Get all food elements"""
+    try:
+        food=Food.query.all()
+        return  jsonify([e.serialize() for e in food])
+    except Exception as e:
+        return(str(e))
+
+@app.route("/get/<id_>")
+def get_by_id(id_):
+    """Get food by id"""
+    try:
+        food=Food.query.filter_by(id=id_).first()
+        return jsonify(food.serialize())
+    except Exception as e:
+        return(str(e))
 
 if __name__ == "__main__":
     app.run()
--- settings/config.py
+++ settings/config.py
@@ -1,6 +1,6 @@
 import os
 
-basedir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 class Config(object):
     DEBUG = False
